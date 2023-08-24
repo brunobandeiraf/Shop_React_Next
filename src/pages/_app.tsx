7760a1be1c1e0014c7b45781b0412fae660baad6
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-24 18:16:20 -0300

    cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from "../styles/global"
+// _app é como o container da aplicação
+
+import logoImg from "../assets/logo.svg"
+
+import { Container, Header } from "../styles/pages/app"
 
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
